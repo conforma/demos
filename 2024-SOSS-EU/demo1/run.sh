769c3e271056867a9c2d418d6ebf304e68840e98
Zoran Regvart <zoran@regvart.com>
2024-09-17 15:10:37 +0200

    Add cosign verify-attestation example to demo1

--- 2024-SOSS-EU/demo1/run.sh
+++ 2024-SOSS-EU/demo1/run.sh
@@ -9,3 +9,4 @@ pe "ec validate image --image quay.io/konflux-ci/ec-golden-image:latest --public
 pe "cat public.key"
 pe "cosign tree quay.io/konflux-ci/ec-golden-image:latest"
 pe "cosign verify --key public.key --insecure-ignore-tlog=true quay.io/konflux-ci/ec-golden-image:latest"
+pe "cosign verify-attestation --key public.key --type slsaprovenance02 --insecure-ignore-tlog=true quay.io/konflux-ci/ec-golden-image:latest 2>&1 | cut -c 1-200"
