f45367c226022609d863c61de8ee2aef8b059fe0
Simon Baird <simon.baird@gmail.com>
2026-02-10 17:11:34 -0500

    Demo content tweaks/improvements/polish

--- Jan-2026/01-validate-input-basics.sh
+++ Jan-2026/01-validate-input-basics.sh
@@ -2,11 +2,17 @@
 set -euo pipefail
 source _helpers.sh
 
-title "Validating arbitary data" "Simon Baird"
+title "Validating arbitrary data" "Simon Baird"
 
-show-msg "In this tutorial we'll introduce some basic Conforma concepts
-and work through some examples of applying policies against some arbitrary
-input data."
+show-msg "In this tutorial we'll introduce some basic Conforma concepts and
+look at examples where Conforma is used to apply policy checks against
+arbitrary input data."
+
+show-msg $'We often use the `ec validate image` command, which fetches and
+verifies an image\'s SLSA provenance attestations, then applies policy checks
+against them. But Conforma can work just as well with any kind of input using
+the `ec validate input` command, and in fact that is a useful way to
+demonstrate some Conforma ideas and techniques.'
 
 h1 "ec validate input"
 
@@ -39,8 +45,7 @@ create-file no-cats/main.rego 'package main
 # description: Disallow felines.
 # custom:
 #   short_name: no_cats
-#   solution: Please ensure no cats are
-#     present in the animal list.
+#   solution: Ensure no cats are present in the animal list!
 #
 deny contains result if {
   some animal in input.animals
@@ -53,7 +58,7 @@ show-rego no-cats/main.rego
 
 pause
 
-show-msg "To use that, Conforma needs a policy file specifying a policy source:"
+show-msg "To use that policy, Conforma needs a policy.yaml file specifying a source:"
 
 create-file policy.yaml 'sources:
 - policy:
@@ -72,7 +77,7 @@ h1 "Using --info for more detailed output"
 
 show-msg 'The metadata associated with the policy rule is important for
 Conforma. Adding the `--info` flag will use the metadata to show more
-verbose/user-friendly output:'
+details about the violation:'
 
 pause-then-run 'ec validate input --file input.yaml --policy policy.yaml --info'
 
@@ -119,11 +124,13 @@ warn contains result if {
 }
 '
 
-show-msg "(Append to the existing file...)"
+show-msg "(We'll append to the existing file here.)"
 
-show-rego no-cats/main.rego -r17: '17,$p'
+show-rego no-cats/main.rego -r16: '16,$p'
 
 pause
+
+show-msg "Notice we now see the warning in the output:"
 
 pause-then-run 'ec validate input --file input.yaml --policy policy.yaml --info --show-successes'
 
@@ -133,7 +140,9 @@ h1 "Adding more detail to the violation reason"
 
 sed -i 's/"No cats allowed!"/sprintf("A cat named %s was found!", [animal.name])/' no-cats/main.rego
 
-show-rego no-cats/main.rego "-r11:15 -H14:14" "11,15p"
+show-msg "Rego is an expressive and capable language so we can easily add more detail to the violation reason. For example:"
+
+show-rego no-cats/main.rego "-r10:14 -H13:13" "10,14p"
 
 pause-then-run 'ec validate input --file input.yaml --policy policy.yaml'
 
@@ -149,19 +158,27 @@ show-yaml input.yaml -H8:
 
 pause-then-run 'ec validate input --file input.yaml --policy policy.yaml'
 
+show-msg $'That\'s about it for this lesson. Hopefully you now have a better
+idea of what Conforma policies look like, and what kind of output Conforma
+produces.'
+
+show-msg $'Before we wrap this up let\'s look at two extra tips which should be
+useful when integrating these kind of policy checks into a CI or build system:'
+
 h1 "Machine readable output"
 
 show-msg 'Text output is the default, but you can also output json or yaml,
-which includes some additional information.'
+which includes some additional information not included in the text output:'
 
-pause-then-run 'ec validate input --file input.yaml --policy policy.yaml --info --output json'
+pause-then-run 'ec validate input --file input.yaml --policy policy.yaml --info --output json | fold -s -w 400'
 
-h1 "Strict vs non-strict"
+
+h1 '"Strict" vs "non-strict"'
 
 show-msg 'By default we produce a non-zero exit code if there are any
 violations, which is useful to interrupt a script or a CI task. You can change
-that behavior if you need to with --strict=false:'
+that behavior if you need to with `--strict=false:`'
 
-show-then-run 'ec validate input --file input.yaml --policy policy.yaml > output.txt; echo "Exit code: $?"; head -3 output.txt'
+pause-then-run 'ec validate input --file input.yaml --policy policy.yaml > output.txt; echo "Exit code: $?"; head -3 output.txt'
 
-show-then-run 'ec validate input --file input.yaml --policy policy.yaml --strict=false > output.txt; echo "Exit code: $?"; head -3 output.txt'
+pause-then-run 'ec validate input --file input.yaml --policy policy.yaml --strict=false > output.txt; echo "Exit code: $?"; head -3 output.txt'
